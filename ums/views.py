a4ae37cb26342db4eeb8a9211aeb70023035952e
olabanjy <shola.albert@gmail.com>
2025-10-03 11:27:05 +0100

    fixes

--- ums/views.py
+++ ums/views.py
@@ -187,7 +187,7 @@ def fetch_stats(request):
 
 
     # upstream
-    upstream = subscriptions.filter(telco_ref__icontains="upstream_paid")
+    upstream = subscriptions.filter(telco_ref__icontains="upstream_paid").distinct("phone")
 
     # Compose the final response
     data = {
