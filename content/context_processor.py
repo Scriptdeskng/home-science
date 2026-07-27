34ee96fc4d41b0af9989081925b7ea9940212e58
Michael Olusoji <olusoji200@gmail.com>
2026-07-27 12:08:27 +0100

    fix

--- content/context_processor.py
+++ content/context_processor.py
@@ -2,5 +2,4 @@ from ums.utils import resolve_msisdn_from_request
 
 
 def fetch_msisdn(request):
-    msisdn = resolve_msisdn_from_request(request)
-    return {"msisdn": msisdn or "Start Watching"}
+    return {"msisdn": resolve_msisdn_from_request(request)}
